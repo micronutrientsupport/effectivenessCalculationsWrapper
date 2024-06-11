8a02ccc7d83372a9fc068071348b11a8b130742a
Bean, Andrew K <andan@bsg.ac.uk>
2024-06-11 11:43:31 +0100

    feat: initial function to trigger baselineAfe

---
+++ R/effectivenessCalculations.R
@@ -0,0 +1,22 @@
+#' @title Triggers call to calculateBaselineInadequacyAfe
+#'
+#' @description
+#' Do thing
+#'
+#' @details
+#' This function can be used to do some things
+#'
+#' @param survey the id of the survey for HCES/FCT data
+#' @param intake_thresholds the intake thresholds for the current intervention
+#'
+#' @return output
+#'
+#' @export
+calculateBaselineInadequacyAfe <- function(survey, intake_thresholds) {
+  .libPaths( "/usr/local/lib/opencpu/apps/ocpu_github_dzvoti_effectivenessCalculations" )
+  mapsdata::loadData(survey)
+  return effectivenessCalculations::calculateBaselineInadequacyAfe(householdConsumptionDf = householdConsumption, householdDetailsDf = householdDetails, nctListDf = nctList, intakeThresholdsDf = intakeThresholds)
+}
+
+#sudo chgrp -R www-data /usr/local/lib/R/site-library/ 
+#sudo chmod -R g+w  /usr/local/lib/R/site-library/ 

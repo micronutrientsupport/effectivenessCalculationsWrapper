a02adcec0b44c7b735a0b19d35ffdc3134a3e94a
Bean, Andrew K <andan@bsg.ac.uk>
2024-06-11 14:45:31 +0100

    feat: add handlers for extra functions

--- R/effectivenessCalculations.R
+++ R/effectivenessCalculations.R
@@ -12,11 +12,51 @@
 #' @return output
 #'
 #' @export
-calculateBaselineInadequacyAfe <- function(survey, intake_thresholds) {
+calculateBaselineInadequacyAfe <- function(survey, micronutrient, intake_thresholds) {
   .libPaths( "/usr/local/lib/opencpu/apps/ocpu_github_dzvoti_effectivenessCalculations" )
   mapsdata::loadData(survey)
-  return effectivenessCalculations::calculateBaselineInadequacyAfe(householdConsumptionDf = householdConsumption, householdDetailsDf = householdDetails, nctListDf = nctList, intakeThresholdsDf = intakeThresholds)
+  result <- effectivenessCalculations::calculateBaselineInadequacyAfe(householdConsumptionDf = householdConsumption, householdDetailsDf = householdDetails, nctListDf = nctList, intakeThresholdsDf = intakeThresholds, MNList = micronutrient)
+  return(result)
 }
 
-#sudo chgrp -R www-data /usr/local/lib/R/site-library/ 
-#sudo chmod -R g+w  /usr/local/lib/R/site-library/ 
+#' @title Triggers call to calculateBaselineInadequacyCnd
+#'
+#' @description
+#' Do thing
+#'
+#' @details
+#' This function can be used to do some things
+#'
+#' @param survey the id of the survey for HCES/FCT data
+#' @param intake_thresholds the intake thresholds for the current intervention
+#'
+#' @return output
+#'
+#' @export
+calculateBaselineInadequacyCnd <- function(survey, micronutrient, intake_thresholds) {
+  .libPaths( "/usr/local/lib/opencpu/apps/ocpu_github_dzvoti_effectivenessCalculations" )
+  mapsdata::loadData(survey)
+  result <- effectivenessCalculations::calculateBaselineInadequacyCND(householdConsumptionDf = householdConsumption, householdDetailsDf = householdDetails, nctListDf = nctList, intakeThresholdsDf = intakeThresholds, MNList = micronutrient)
+  return(result)
+}
+
+#' @title Triggers call to calculate_pre_and_post_lsff_summaries_afe
+#'
+#' @description
+#' Do thing
+#'
+#' @details
+#' This function can be used to do some things
+#'
+#' @param survey the id of the survey for HCES/FCT data
+#' @param intake_thresholds the intake thresholds for the current intervention
+#'
+#' @return output
+#'
+#' @export
+calculatePreAndPostLSFFSummaries <- function(survey, micronutrient, intake_thresholds) {
+  .libPaths( "/usr/local/lib/opencpu/apps/ocpu_github_dzvoti_effectivenessCalculations" )
+  mapsdata::loadData(survey)
+  result <- effectivenessCalculations::calculate_pre_and_post_lsff_summaries_afe(householdConsumptionDf = householdConsumption, householdDetailsDf = householdDetails, nctListDf = nctList, intakeThresholdsDf = intakeThresholds, MNList = micronutrient)
+  return(result)
+}

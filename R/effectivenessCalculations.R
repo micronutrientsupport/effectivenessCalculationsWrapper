ac338ea166e28f1361c085b3e83bc74beb971fb0
Bean, Andrew K <andan@bsg.ac.uk>
2024-06-12 14:13:43 +0100

    feat: update summary call

--- R/effectivenessCalculations.R
+++ R/effectivenessCalculations.R
@@ -54,14 +54,27 @@ calculateBaselineInadequacyCnd <- function(survey, micronutrient, intakeThreshol
 #' @return output
 #'
 #' @export
-calculatePreAndPostLSFFSummaries <- function(survey, micronutrient, intakeThresholds) {
+calculatePreAndPostLSFFSummaries <- function(survey, micronutrient, intakeThresholds, fortifiableFoodItems, fortificationLevels) {
   .libPaths( "/usr/local/lib/opencpu/apps/ocpu_github_dzvoti_effectivenessCalculations" )
   mapsdata::loadData(survey)
-  result <- effectivenessCalculations::calculate_pre_and_post_lsff_summaries_afe(householdConsumptionDf = householdConsumption, householdDetailsDf = householdDetails, nctListDf = nctList, intakeThresholdsDf = intakeThresholds, MNList = micronutrient)
+  result <- effectivenessCalculations::calculate_pre_and_post_lsff_summaries_afe(householdConsumptionDf = householdConsumption, householdDetailsDf = householdDetails, nctListDf = nctList, intakeThresholdsDf = intakeThresholds, MNList = micronutrient, fortifiableFoodItemsDf = fortifiableFoodItems, fortificationLevelsDf = fortificationLevels)
   return(result)
 }
 
-
+#' @title Triggers call to calculate_pre_and_post_lsff_summaries_afe
+#'
+#' @description
+#' Do thing
+#'
+#' @details
+#' This function can be used to do some things
+#'
+#' @param survey the id of the survey for HCES/FCT data
+#' @param intake_thresholds the intake thresholds for the current intervention
+#'
+#' @return output
+#'
+#' @export
 calculateBaselinePrevalenceHighIntakeRiskAfe <- function(survey, micronutrient, intakeThresholds) {
   .libPaths( "/usr/local/lib/opencpu/apps/ocpu_github_dzvoti_effectivenessCalculations" )
   mapsdata::loadData(survey)
